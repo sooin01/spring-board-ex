11a43fa28b9170731c2db01bcf8d1d4da2d37dd5
sooin01 <sooin01@gmail.com>
2019-09-25 21:46:24 +0900

    add

--- src/main/java/com/my/app/common/util/CmdUtil.java
+++ src/main/java/com/my/app/common/util/CmdUtil.java
@@ -9,17 +9,26 @@ public class CmdUtil {
 	public static void main(String[] args) throws Exception {
 		String[] commonds = { "C:\\Users\\sooin\\Desktop\\hello.cmd", "parameter 1", "parameter 2" };
 		Process process = Runtime.getRuntime().exec(commonds);
-//		InputStream is = process.getErrorStream();
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream(), "UTF-8"));
 		String line = null;
+
+		// 표준출력
+		BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream(), "UTF-8"));
 		while ((line = br.readLine()) != null) {
-			System.out.println(line);
+			System.out.println("OUTPUT: " + line);
 		}
 		br.close();
 
-		boolean waitFor = process.waitFor(15, TimeUnit.SECONDS);
-		System.out.println("waitFor: " + waitFor);
+		// 표준에러
+		BufferedReader er = new BufferedReader(new InputStreamReader(process.getErrorStream(), "UTF-8"));
+		while ((line = er.readLine()) != null) {
+			System.out.println("ERROR: " + line);
+		}
+		er.close();
+
+		boolean waitFor = process.waitFor(15, TimeUnit.SECONDS); // true면 정상종료. false면 timeout으로 인한 종료.
+		int exitValue = process.exitValue(); // 0이면 정상. 그 외는 에러.
+		System.out.println("waitFor: " + waitFor + ", exitValue: " + exitValue);
 	}
 
 }
@@ -32,3 +41,5 @@ public class CmdUtil {
 //
 //cd C:\Users\sooin\Desktop
 //"C:\Program Files\Java\jdk1.8.0_221\bin\java" Test
+//
+//exit 0(default)
