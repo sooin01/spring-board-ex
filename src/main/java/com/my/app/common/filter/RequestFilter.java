e65f7f67166d0ab43ad1f264c8c61b6881036bb7
sooin01 <sooin01@gmail.com>
2019-11-20 23:14:34 +0900

    add

---
+++ src/main/java/com/my/app/common/filter/RequestFilter.java
@@ -0,0 +1,23 @@
+package com.my.app.common.filter;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+
+public class RequestFilter extends OncePerRequestFilter {
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		ContentCachingRequestWrapper requestWrapper = new ContentCachingRequestWrapper(request);
+
+		filterChain.doFilter(requestWrapper, response);
+	}
+
+}
